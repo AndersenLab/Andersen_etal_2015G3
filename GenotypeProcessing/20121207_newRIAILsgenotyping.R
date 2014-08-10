e3ab8ab632eee96026317a9adbf7a9f06bcff024
Tyler Shimko <TShimko126@gmail.com>
2014-08-09 20:28:46 -0500

    Add comments and streamline code

--- GenotypeProcessing/20121207_newRIAILsgenotyping.R
+++ GenotypeProcessing/20121207_newRIAILsgenotyping.R
@@ -1,26 +1,36 @@
 library(plyr)
 library(dplyr)
 library(ggplot2)
-#Read in the table with matt rockmans SNP set
-rockman <- read.table("~/Downloads/RILS.txt", header=TRUE)
-rockmanSNPs <- rockman$SNP_ID
-troubleSNPs <- c("UCE6-686", "UCE4-528", "CE4-241", "UCE6-712", "UCE6-1285", "CE5-218")
+library(mclust)
+library(qtl)
 
-#2012 12 07 I will process my RIAILs using EM and clean up with rQTL
+####################################
+#remove this before final packaging#
+####################################
+setwd("~/HTA_Linkage/")
+####################################
+#remove this before final packaging#
+####################################
 
-load("~/Downloads/rawplates.RData")
+#Read in the table with Matt Rockmans SNP set
+rockmanData <- read.table("GenotypeProcessing/RILS.txt", header=TRUE)
+rockmanSNPs <- rockmanData$SNP_ID
 
-#Now, I need to put in strain assignments for the raw data from Rockefeller.
+# Read in the raw GoldenGate genotyping files
 
-strains <- read.csv("~/Downloads/GG_strains.csv", header=F, stringsAsFactors=F)[,1]
+load("GenotypeProcessing/rawplates.RData")
+
+# Put in strain assignments for the raw data from Rockefeller
+
+strains <- read.csv("GenotypeProcessing/GG_strains.csv", header=F, stringsAsFactors=F)[,1]
 
 plate1$strains <- rep(strains[1:96], each=1536)
 plate2$strains <- rep(strains[97:192], each=1536)
 plate3$strains <- rep(strains[289:384], each=1536)
 plate4$strains <- rep(strains[193:288], each=1536)
 
-#Note, plate 3 and plate 4 assignments are switched above to fix the plate mixup at Rockefeller. I can only
-#really see mixups with plate 4 because there are non-RIAILs on that plate. Plates 1 and 2 are correct.
+# Note, plate 3 and plate 4 assignments are switched above to fix the plate mixup at Rockefeller. Can only
+# really see mixups with plate 4 because there are non-RIAILs on that plate. Plates 1 and 2 are correct.
 
 plate1$plate <- 1
 plate2$plate <- 2
@@ -29,78 +39,69 @@ plate4$plate <- 4
 
 all <- rbind(plate1, plate2, plate3, plate4)
 
-#Reduce to only useful data
+# Reduce to only useful data (strains, SNP names, positions, and raw genotype data)
 
 gg <- with(all, data.frame(strain = strains, snp=SNP.Name, chr=Chr, pos=Position, x=X.Raw, y=Y.Raw))
 
-#Here is where you can look at the plots of raw values to see how genotypes were assigned. 
-
-gg %>% filter(., snp == "CE3-140") %>% ggplot(.) + aes(x=x, y=y) + geom_point()
-gg %>% ggplot(.) + aes(x=x, y=y) + geom_point()
-
-#Genotypes at each SNP can be assigned as a mixture of two normal distributions. Individuals outside of defined groups
-#can be called NA and cleaned up with the HMM later.
-
-library(mclust)
-
-#Make an error vector for the tryCatch
+# Make an error vector for the tryCatch (sets all classification values to NA)
 
 mclust.error <- Mclust(data=gg[gg$snp=="CE1-28", c("x","y")], G=2, modelNames=c("VVV"), warn=T)
 
 mclust.error$classification[1:384] <- rep(NA, 384)
 
-#Assign to genotypes using EM algorithm. It takes a few minutes.
+# Assign genotypes to raw data using EM algorithm. This step takes a few minutes.
 
 all.clusters <- dlply(.data=gg, .variables=.(snp), .fun=function(D) {
-  tryCatch({out<-Mclust(data=D[,c("x","y")], G=2, modelNames=c("VVV"), warn=T)
-            #out$data<-D[,c("strain", "snp", "chr", "pos")]
-            return(out)}, error=function(x){print(D$snp[1]); return(mclust.error)})
+    tryCatch({out <- Mclust(data=D[,c("x","y")], G=2, modelNames=c("VVV"), warn=T)
+              #out$data<-D[,c("strain", "snp", "chr", "pos")]
+              return(out)}, error=function(x){print(D$snp[1]); return(mclust.error)})
 })
 
-#Bad marker: UCE2-2369
+# Turn the values into numeric for the two values of "z" and that of classification
 
-#Turn into numeric
+z1 <- sapply(all.clusters, function(x){as.numeric(x$z[,1])})
+z2 <- sapply(all.clusters, function(x){as.numeric(x$z[,2])})
+calls <- sapply(all.clusters, function(x){as.numeric(x$classification)})
 
-z1<-sapply(all.clusters, function(x){as.numeric(x$z[,1])})
-z2<-sapply(all.clusters, function(x){as.numeric(x$z[,2])})
-calls<-sapply(all.clusters, function(x){as.numeric(x$classification)})
-
-#Look at the data in log likelihood space
+# Look at the data in log likelihood space
 
 hist(log10(z1/z2), breaks=10000, xlim=c(-5, 5))
 
-#Bad calls are likely in the middle around 0 from -1 to 1. Turn them into NA
-
-sum(log10(z1/z2)>-1&log10(z1/z2)<1)/length(z1)
+# Bad calls are likely in the middle around 0 from -1 to 1. Turn them into NA.
 
 indNA <- which(log10(z1/z2)>-1&log10(z1/z2)<1, arr.ind=T)
 
 calls[indNA] <- NA
 
-#Now, look to see how genotype calls look.
+# Create SNPs x Strains matrix
 
 rownames(calls) <- as.character(unique(gg$strain))
 
 calls <- t(calls)
 
+# Polarize the genome by N2 (Bristol, "B") or CB4856 (Hawaiian, "H") alleles based on QG1
+
 qg <- as.numeric(calls[,"QG1"])
 
 qg.polarize <- function(col){
-  BH <- ifelse(col == qg, "B", "H")
-  return(BH)
+    BH <- ifelse(col == qg, "B", "H")
+    return(BH)
 }
 
 n.calls <- apply(calls, 2, qg.polarize)
 
+# Check that none of the QG1 SNPs were misclassified as "H". The following line should = 0.
+
 sum(n.calls[,"QG1"] == "H", na.rm=T)
+
+# Get the chromosome and position values for each SNP
 
 chrom <- as.numeric(gg$chr[match(rownames(n.calls), gg$snp)])
 pos <- gg$pos[match(rownames(n.calls), gg$snp)]
 
+# Order the calls based on chomosome and position
+
 ord.calls <- n.calls[order(chrom, pos),]
-
-plot(as.numeric(as.factor(ord.calls[,"QX1436 6X N2"])), type="b")
-
 ord.chrom <- chrom[order(chrom, pos)]
 ord.pos <- pos[order(chrom, pos)]
 
@@ -108,54 +109,62 @@ ord.id <- paste(ord.chrom, ord.pos, rownames(ord.calls), sep="_")
 
 rownames(ord.calls) <- ord.id
 
+# Add in identifying info for the RIAILs
+
 rils <- paste("QX", seq(240, 598), sep="")
 
 ril.ind <- match(rils, colnames(ord.calls))
 
-forqtl <- data.frame(id = rownames(ord.calls), chrom=ord.chrom, ord.calls[,ril.ind], stringsAsFactors=F)
+# Make the final genotype table for the the cross object and save it to be incorporated into a cross object later
 
-write.table(forqtl, file="~/Downloads/fullgeno_newrils.csvsr", sep="\t", row.names=F, quote=F)
+forqtl <- data.frame(id = seq(240, 598), chrom=ord.chrom, ord.calls[,ril.ind], stringsAsFactors=F)
+
+write.table(forqtl, file="GenotypeProcessing/fullgeno_newrils.csvsr", sep="\t", row.names=F, quote=F)
+
+# Make a dummy phenotype file for the cross object
 
 pheno <- forqtl[c(1,2),-2]
 
 pheno[1,-1] <- rep(5, ncol(pheno)-1) 
 pheno[2,-1] <- rep(1, ncol(pheno)-1) 
 
-write.table(pheno, file="~/Downloads/pheno_newrils.csvsr", sep="\t", row.names=F, quote=F)
+write.table(pheno, file="GenotypeProcessing/pheno_newrils.csvsr", sep="\t", row.names=F, quote=F)
 
-##############------------------------------------------########################
+######################## Begin testing the cross object ########################
 
-library(qtl)
+# Set the location of the phenotype and genotype files
 
-gfile <- "~/Downloads/fullgeno_newrils.csvsr"
-pfile <- "~/Downloads/pheno_newrils.csvsr"
+gfile <- "GenotypeProcessing/fullgeno_newrils.csvsr"
+pfile <- "GenotypeProcessing/pheno_newrils.csvsr"
 
-gfile <- "~/Downloads/imputedgeno.csvsr"
+# Define a function to read genotype and phenotype info into a cross object for R/qtl
 
-readCross = function(g,p) {   return( 
-  read.cross(format='csvsr', 
-             genfile = g,
-             phefile = p,
-             na.strings='NA', genotypes=c('B','H'), alleles=c('B','H'), 
-             sep='\t', estimate.map=FALSE, comment.char='')   ) 
+readCross = function(g,p) {
+    return( 
+        read.cross(format='csvsr', 
+                   genfile = g,
+                   phefile = p,
+                   na.strings='NA', genotypes=c('B','H'), alleles=c('B','H'), 
+                   sep='\t', estimate.map=FALSE, comment.char='')) 
 }
+
+# Create the cross object with readCross
 
 cr <- readCross(gfile, pfile)
 
-#Let's eliminate genotype skews
+# Plot all of the genotype skews for "BB"
 
 gt <- geno.table(cr, scanone=T)
-head(gt)
 
 ggplot(data=gt) + aes(x=pos, y=BB) + geom_point() + facet_grid(chr~.) +
-  geom_hline(yintercept=0.5, color="red") + 
-  geom_hline(yintercept=0.45, color="blue") + 
-  geom_hline(yintercept=0.4, color="orange") + 
-  geom_hline(yintercept=0.35, color="purple") + 
-  geom_hline(yintercept=0.3, color="green") + 
-  geom_hline(yintercept=0.25, color="yellow")
+    geom_hline(yintercept=0.5, color="red") + 
+    geom_hline(yintercept=0.45, color="blue") + 
+    geom_hline(yintercept=0.4, color="orange") + 
+    geom_hline(yintercept=0.35, color="purple") + 
+    geom_hline(yintercept=0.3, color="green") + 
+    geom_hline(yintercept=0.25, color="yellow")
 
-#Make allele frequency cuts...BB<0.25 is a good general cutoff
+# Make allele frequency cuts individually for each chromosome for "BB"
 
 cr$geno$"1"$data[,colnames(cr$geno$"1"$data) %in% rownames(gt[which(gt$BB<0.35),])] <- NA
 cr$geno$"2"$data[,colnames(cr$geno$"2"$data) %in% rownames(gt[which(gt$BB<0.35),])] <- NA
@@ -164,23 +173,17 @@ cr$geno$"4"$data[,colnames(cr$geno$"4"$data) %in% rownames(gt[which(gt$BB<0.35),
 cr$geno$"5"$data[,colnames(cr$geno$"5"$data) %in% rownames(gt[which(gt$BB<0.3),])] <- NA
 cr$geno$"6"$data[,colnames(cr$geno$"6"$data) %in% rownames(gt[which(gt$BB<0.3),])] <- NA
 
-gt2 <- geno.table(cr, scanone=T)
+# Plot all of the genotype skews for "BH"
 
-ggplot(data=gt2) + aes(x=pos, y=BB) + geom_point() + facet_grid(chr~.) +
-  geom_hline(yintercept=0.5, color="red") + 
-  geom_hline(yintercept=0.45, color="blue") + 
-  geom_hline(yintercept=0.4, color="orange") + 
-  geom_hline(yintercept=0.35, color="purple") + 
-  geom_hline(yintercept=0.3, color="green") + 
-  geom_hline(yintercept=0.25, color="yellow")
+ggplot(data=gt) + aes(x=pos, y=BH) + geom_point() + facet_grid(chr~.) +
+    geom_hline(yintercept=0.5, color="red") + 
+    geom_hline(yintercept=0.45, color="blue") + 
+    geom_hline(yintercept=0.4, color="orange") + 
+    geom_hline(yintercept=0.35, color="purple") + 
+    geom_hline(yintercept=0.3, color="green") + 
+    geom_hline(yintercept=0.25, color="yellow")
 
-ggplot(data=gt2) + aes(x=pos, y=BH) + geom_point() + facet_grid(chr~.) +
-  geom_hline(yintercept=0.5, color="red") + 
-  geom_hline(yintercept=0.45, color="blue") + 
-  geom_hline(yintercept=0.4, color="orange") + 
-  geom_hline(yintercept=0.35, color="purple") + 
-  geom_hline(yintercept=0.3, color="green") + 
-  geom_hline(yintercept=0.25, color="yellow")
+# Make allele frequency cuts individually for each chromosome for "BH"
 
 cr$geno$"2"$data[,colnames(cr$geno$"2"$data) %in% rownames(gt[which(gt$BH<0.4),])] <- NA
 cr$geno$"3"$data[,colnames(cr$geno$"3"$data) %in% rownames(gt[which(gt$BH<0.4),])] <- NA
@@ -188,35 +191,17 @@ cr$geno$"4"$data[,colnames(cr$geno$"4"$data) %in% rownames(gt[which(gt$BH<0.4),]
 cr$geno$"5"$data[,colnames(cr$geno$"5"$data) %in% rownames(gt[which(gt$BH<0.4),])] <- NA
 cr$geno$"6"$data[,colnames(cr$geno$"6"$data) %in% rownames(gt[which(gt$BH<0.4),])] <- NA
 
+# Eliminate addition troublesome SNPs that imputation does not fix
+
 cr$geno$"1"$data[,colnames(cr$geno$"1"$data) == "1_13432384_UCE1-1433"] <- NA
 cr$geno$"5"$data[,colnames(cr$geno$"5"$data) == "5_19393678_UCE5-3001"] <- NA
 cr$geno$"6"$data[,colnames(cr$geno$"6"$data) == "6_1663691_UCE6-686"] <- NA
 
-
-gt3 <- geno.table(cr, scanone=T)
-
-ggplot(data=gt3) + aes(x=pos, y=BH) + geom_point() + facet_grid(chr~.) +
-  geom_hline(yintercept=0.5, color="red") + 
-  geom_hline(yintercept=0.45, color="blue") + 
-  geom_hline(yintercept=0.4, color="orange") + 
-  geom_hline(yintercept=0.35, color="purple") + 
-  geom_hline(yintercept=0.3, color="green") + 
-  geom_hline(yintercept=0.25, color="yellow")
-
-
-
-
-
-
-
-
-
-
-
-
-cr2 <- cr
+# Get all of the SNP names
 
 allSnps <- c(colnames(cr$geno$"1"$data), colnames(cr$geno$"2"$data), colnames(cr$geno$"3"$data), colnames(cr$geno$"4"$data), colnames(cr$geno$"5"$data), colnames(cr$geno$"6"$data))
+
+# Get the set of SNPs not in the Rockman set (Rockman, et al. 2009) and drop these SNPs from the cross object
 
 keepSnps <- c()
 
@@ -227,155 +212,13 @@ for(i in rockmanSNPs){
 
 removeSnps <- allSnps[!(allSnps %in% keepSnps)]
 
-cr2 <- drop.markers(cr2, removeSnps)
+cr <- drop.markers(cr, removeSnps)
 
-# argmax.geno() here, play with error prob
-icr <- argmax.geno(cr2, error.prob=.00001)
+# Impute genotypes on the cross object
 
+icr <- argmax.geno(cr, error.prob=.00001)
 
-
-
-View(filter(gt, chr==1))
-
-
-
-
-
-
-
-
-
-
-
-a <- data.frame(icr$geno$"1"$argmax)
-
-
-gt4 <- geno.table(icr, scanone=T)
-gt4$chr <- factor(gt4$chr, labels=c("I", "II", "III", "IV", "V", "X"))
-
-gt4$SNP <- do.call(rbind, strsplit(rownames(gt4), "_"))[,3]
-
-gt4 <- filter(gt4, SNP %in% rockmanSNPs)
-
-load("markers.Rda")
-
-markers2 <- select(markers, -chr.num, -chr.roman)
-gt4 <- merge(gt4, markers2, by="SNP")
-
-write.table(gt4, file="~/Downloads/newrils_genotable.txt", sep="\t", quote=F, row.names=F, col.names=T)
-
-ggplot(data=gt4) + aes(x=WS185.pos/1e6, y=BB) + geom_line() + geom_point() +
-  facet_grid(chr~., scales="free_x") +
-  xlab("Genomic position (Mb)") + ylab("Bristol allele frequency") +
-  theme(plot.title=element_text(size=18, face="bold"), 
-        axis.title.x = element_text(vjust=0, size=16, face="bold"), 
-        legend.position="none", 
-        axis.title.y=element_text(size=16, angle=90, face="bold"), 
-        axis.text.x=element_text(size=14, face="bold", color="black"), 
-        axis.text.y=element_text(size=14, face="bold", color="black"), 
-        strip.text.x=element_text(size=14, face="bold"),
-        strip.text.y=element_text(size=14, face="bold", angle=90))
-
-#Skews are effectively gone.
-
-#Now, check for similarities in genotype
-
-cg <- comparegeno(cr)
-
-hist(cg, breaks=100, xlab="Fraction of genome shared", main="")
-abline(v=0.5, col="red", lty=2, lwd=3)
-rug(cg)
-
-which(cg>0.9, arr.ind=T)
-
-#There are no similar individuals.
-
-#Now, check recombination fraction.
-
-cr <- est.rf(cr)
-
-checkAlleles(cr) #1_316117_UCE1-530 is likely switched.
-
-g <- pull.geno(cr, 1)
-g[,"1_316117_UCE1-530"] <- abs(g[,"1_316117_UCE1-530"] - 3)
-cr$geno[[1]]$data <- g
-
-#This has to be done again for no apparent errors to be found
-cr <- est.rf(cr)
-
-checkAlleles(cr) #No apparent problems. It's fixed.
-
-plotRF(cr)
-
-#There are some markers out of place, but better than Matt's set.
-
-cr.map <- est.map(cr, verbose=T, error.prob=0.06, tol=1e-5)
-
-plot.map(cr.map)
-
-#Chromosome map looks pretty bad. Let's try to impute and see what happens.
-
-imputeMissingGeno  = function(cross) {
-  impcross = argmax.geno(cross, error.prob=0.06)
-  impcross$geno = lapply(impcross$geno, function(x){x$data = x$argmax; x$argmax=NULL; return(x) })
-  #dm = findDupMarkers(impcross, exact.only = FALSE , adjacent.only = FALSE )
-  #names(dm)=NULL
-  #impcross = drop.markers(impcross, unlist(dm))
-  return(impcross) }
-
-icr <- imputeMissingGeno(cr)
-
-icr <- est.rf(icr)
-
-checkAlleles(icr) #No apparent problems.
-
-plotRF(icr) #There might be some more LD between 1 and 2, but otherwise way better.
-
-icr.map <- est.map(icr, verbose=T, error.prob=0.06, tol=1e-5)
-
-newrils.impute <- icr
-
-plot.map(icr.map) #Map looks great. Imputation smoothed over bad SNPs from before.
-
-#I need to make a position file for distribution to labs to do their own mapping.
-library(stringr)
-
-snplist <- c(colnames(icr$geno$"1"$data), colnames(icr$geno$"2"$data), colnames(icr$geno$"3"$data),
-      colnames(icr$geno$"4"$data), colnames(icr$geno$"5"$data), colnames(icr$geno$"6"$data))
-
-sp.snplist <- str_split_fixed(snplist, "_", 3)
-
-sp.snplist[,1] <- ifelse(sp.snplist[,1]=="6", "X", sp.snplist[,1])
-
-snps <- data.frame(fullsnp = as.character(snplist), snp = as.character(sp.snplist[,3]), chr = as.factor(sp.snplist[,1]), pos = as.numeric(sp.snplist[,2]), stringsAsFactors=F)
-
-setwd("~/Dropbox/GoldenGate for new RIAILs/")
-
-save(snps, newrils.impute, file="newrils.RData")
-
-#Let's try a mapping
-
-names(newrils.impute$geno) <- chrnames
-
-newrils.impute <- calc.genoprob(newrils.impute)
-
-newqx <- paste("QX", seq(240, 598), sep="")
-
-red.newrils <- subset(newrils.impute, chr=chrnames, id = newqx)
-
-red.pq <- subset(all.pq, chr=chrnames, ind = newqx)
-
-comb.new <- red.newrils
-
-comb.new$pheno <- red.pq$pheno
-
-trait <- scanone(comb.new, pheno.col=5, model="np")
-plot(trait)
-
-
-
-
-
+# Pull out the imputed genotypes along with position information and save the new genotype table
 
 genoFile <- lapply(icr$geno, function(x){x$argmax})
 genoFile <- do.call(cbind, genoFile)
@@ -385,4 +228,35 @@ id <- sapply(rownames(genoFile), function(x){strsplit(x, "_")[[1]][3]})
 chrom <- sapply(rownames(genoFile), function(x){strsplit(x, "_")[[1]][1]})
 genoFile <- data.frame(lapply(data.frame(genoFile), function(x){ifelse(x==1, "B", "H")}))
 genoFile <- data.frame(cbind(id, chrom, genoFile))
-write.table(genoFile, file="~/Downloads/imputedgeno.csvsr", sep="\t", row.names=F, quote=F)
+write.table(genoFile, file="GenotypeProcessing/imputedgeno_newrils.csvsr", sep="\t", row.names=F, quote=F)
+
+# Make new cross object from imputed genotype data
+
+pfile <- "GenotypeProcessing/pheno_newrils.csvsr"
+gfile <- "GenotypeProcessing/imputedgeno_newrils.csvsr"
+
+cr2 <- readCross(gfile, pfile)
+
+# Pull out the genotype table
+
+gt2 <- geno.table(cr2, scanone=T)
+
+# Plot the genotype skews. No spikes in the data indicate issues were fixed by imputation.
+
+ggplot(data=gt2) + aes(x=pos, y=BB) + geom_line() + geom_point() +
+    facet_grid(chr~., scales="free_x") +
+    xlab("Genomic position (Mb)") + ylab("Bristol allele frequency") +
+    theme(plot.title=element_text(size=18, face="bold"), 
+          axis.title.x = element_text(vjust=0, size=16, face="bold"), 
+          legend.position="none", 
+          axis.title.y=element_text(size=16, angle=90, face="bold"), 
+          axis.text.x=element_text(size=14, face="bold", color="black"), 
+          axis.text.y=element_text(size=14, face="bold", color="black"), 
+          strip.text.x=element_text(size=14, face="bold"),
+          strip.text.y=element_text(size=14, face="bold", angle=90))
+
+# Rename the cross object to work with the mapping pipeline
+
+N2xCB4856.cross <- cr2
+
+save(N2xCB4856.cross, file="Mapping/N2xCB4856_RIAILs_Rqtlfiles.RData")
